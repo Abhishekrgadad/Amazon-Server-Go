c9378a5ffd7f49e1edb60c888a350498191db5fa
abhishekrg22 <abhishek.g@signdesk.com>
2025-04-24 16:05:28 +0530

    updated styling

--- modules/review/controllers.go
+++ modules/review/controllers.go
@@ -17,30 +17,23 @@ func AddReviewHandler(c *fiber.Ctx) error {
 	if err := c.BodyParser(&req); err != nil {
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid payload"})
 	}
-
 	orderID, err := primitive.ObjectIDFromHex(req.OrderID)
 	if err != nil {
 		return c.Status(400).JSON(fiber.Map{"error": "Invalid order ID"})
 	}
-
 	productID, err := primitive.ObjectIDFromHex(req.ProductID)
 	if err != nil {
 		return c.Status(400).JSON(fiber.Map{"error": "Invalid product ID"})
 	}
-
 	orderCollection := config.DB.Collection("orders")
 	var order order.Order
-
-
 	err = orderCollection.FindOne(context.TODO(), bson.M{
 		"_id":     orderID,
 		"product_id": productID,
 	}).Decode(&order)
-
 	if err != nil {
 		return c.Status(404).JSON(fiber.Map{"error": "Order not found or unauthorized"})
 	}
-
 	productFound := false
 	for _, item := range order.Items {
 		if item.ProductID == productID {
@@ -51,7 +44,6 @@ func AddReviewHandler(c *fiber.Ctx) error {
 	if !productFound {
 		return c.Status(400).JSON(fiber.Map{"error": "Product not found in the order"})
 	}
-
 	review := Review{
 		ID:        primitive.NewObjectID(),
 		ProductID: productID,
@@ -60,12 +52,10 @@ func AddReviewHandler(c *fiber.Ctx) error {
 		Comment:   req.Comment,
 		CreatedAt: time.Now(),
 	}
-
 	_, err = config.DB.Collection("reviews").InsertOne(context.TODO(), review)
 	if err != nil {
 		return c.Status(500).JSON(fiber.Map{"error": "Failed to add review"})
 	}
-
 	productCollection := config.DB.Collection("products")
 
 	var product product.Product
@@ -73,10 +63,8 @@ func AddReviewHandler(c *fiber.Ctx) error {
 	if err != nil {
 		return c.Status(404).JSON(fiber.Map{"error": "Product not found"})
 	}
-
 	newTotalReviews := product.TotalReviews + 1
 	newAverageRating := ((product.AverageRating * float64(product.TotalReviews)) + float64(req.Rating)) / float64(newTotalReviews)
-
 	_, err = productCollection.UpdateOne(
 		context.TODO(),
 		bson.M{"_id": productID},
@@ -90,7 +78,6 @@ func AddReviewHandler(c *fiber.Ctx) error {
 	if err != nil {
 		return c.Status(500).JSON(fiber.Map{"error": "Failed to update product rating"})
 	}
-
 	return c.JSON(fiber.Map{"message": "Review added successfully for the product"})
 }
 
@@ -99,11 +86,9 @@ func GetProductReviewsHandler(c *fiber.Ctx) error {
 	if err != nil {
 		return c.Status(400).JSON(fiber.Map{"error": "Invalid product ID"})
 	}
-
 	reviews, err := GetReviewsByProduct(productID)
 	if err != nil {
 		return c.Status(500).JSON(fiber.Map{"error": "Failed to fetch reviews"})
 	}
-
 	return c.JSON(reviews)
 }
