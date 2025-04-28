ef94aa6c024ceb5e9559781e77feb9300b32eb42
abhishekrgadad <abhishekgadad485@gmail.com>
2025-04-28 23:08:36 +0530

    Added register functions

---
+++ modules/Auth/structures.go
@@ -0,0 +1,10 @@
+package auth
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+type User struct {
+	ID       primitive.ObjectID `bson:"_id,omitempty"`
+	Username string             `json:"username" bson:"username"`
+	Email    string             `json:"email" bson:"email"`
+	Password string             `json:"password" bson:"password"`
+}
