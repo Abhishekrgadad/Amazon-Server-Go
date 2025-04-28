966e9024a3906ac508f99d445bb5781f6e40489c
abhishekrgadad <abhishekgadad485@gmail.com>
2025-04-28 22:49:17 +0530

    setup db

--- main.go
+++ main.go
@@ -1,9 +1,22 @@
 package main
 
-import "github.com/gofiber/fiber/v2" 
+import (
+	"log"
+	"server/config"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
+)
 
 
 func main() {
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	config.ConnectDB()
+	
 	app := fiber.New()
 
 	app.Get("/",func (c *fiber.Ctx) error {
